a7d1d4886f626d46a3a74289fe71077afcde7a3b
Maruko Aizawa <80887472+Mark-Barican@users.noreply.github.com>
2022-10-19 21:56:28 +0800

    bg

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// const tween = KUTE.fromTo(
+//     '#border1',
+//     { path: 'blob1.1'},
+//     { path: 'blob1.2'},
+//     { repeat: 999, duration: 3000, yoyo: true}
+// )
+
+// tween.start()
